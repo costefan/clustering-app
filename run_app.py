19458b9eb7de7d5855edef9ac1780d27e8669379
costefan <costefan@costefans-MacBook-Pro.local>
2018-11-23 18:22:16 +0200

    add port from env vars

--- run_app.py
+++ run_app.py
@@ -1,4 +1,6 @@
+import os
 import asyncio
+
 from aiohttp import web
 
 from app.main import create_app
@@ -6,6 +8,7 @@ from app.main import create_app
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+    port = int(os.getenv('PORT', 8000))
 
     app = create_app(loop)
-    web.run_app(app, port=8000)
+    web.run_app(app, port=port)
